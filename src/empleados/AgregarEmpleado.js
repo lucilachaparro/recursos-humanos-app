591ea9880dc248f088af4a0d65e8bb065284fd7b
Lucila Chaparro <lchaparro@dgrcorrientes.gov.ar>
2024-06-27 16:14:56 -0300

    agregar empleado

--- src/empleados/AgregarEmpleado.js
+++ src/empleados/AgregarEmpleado.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AgregarEmpleado() {
+
+  const [empleado, setEmpleado]=useState({
+    nombre:"",
+    departamento:"",
+    sueldo:""
+  })
+
   return (
-    <div>
-      Agregar Empleado
+    <div className='container'>
+      <div className='container text-center' style={{margin: "30px"}}>
+        <h3>Agregar Empleado</h3>
+      </div>
+      <form>
+      <div className="mb-3">
+        <label htmlFor="nombre" className="form-label">Nombre</label>
+        <input type="text" className="form-control" id="nombre" name="nombre" required={true}/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="departamento" className="form-label">Departamento</label>
+        <input type="text" className="form-control" id="departamento" name="departamento"/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="sueldo" className="form-label">Sueldo</label>
+        <input type="number" step="any" className="form-control" id="sueldo" name="sueldo"/>
+      </div>
+      <div className='text-center'>
+        <button type='submit' className="btn btn-warning btn-sm me-3">Agregar</button>
+        <a href='/' className='btn btn-danger btn-sm'>Regresar</a>
+      </div>
+      <button type="submit" className="btn btn-primary">Agregar</button>
+      </form>
     </div>
   )
 }
