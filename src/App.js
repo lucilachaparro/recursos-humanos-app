450c3fe6dca7c88a45ad67f194abc7b1f53ddbae
Lucila Chaparro <lucilachaparro@gmail.com>
2024-06-26 23:15:47 -0300

    navbar agregar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <div className='container'>
-    <Navegacion />
-    <ListadoEmpleados/>
+    <BrowserRouter>
+     <Navegacion/>
+      <Routes>
+        <Route exact path='/' element={<ListadoEmpleados/>}/>
+        <Route exact path='/agregar' element={<AgregarEmpleado/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
